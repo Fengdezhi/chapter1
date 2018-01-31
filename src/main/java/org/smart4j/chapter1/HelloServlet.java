40f68bcaee75420d395aa20621ee0fd92651f5f8
MACHENIKE <952202052@qq.com>
2018-01-31 18:28:24 +0800

    提交到git2

--- src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -12,6 +12,7 @@ import java.util.Date;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet{
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         DateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
